3d84f0cb51c3a8d02d00ef6f5fd419d72b08ee8c
Elías Moreno <moreno.mata@bootcamp-upgrade.com>
2022-01-22 15:52:29 +0100

    READY FOR DEPLOY

--- src/Components/Preloader/Preloader.jsx
+++ src/Components/Preloader/Preloader.jsx
@@ -7,7 +7,9 @@ const Preloader = () => {
         <section className="preloader-section">
             <div className="preloader-container">
                 <img id="logo" className="animate__animated animate__bounce" src={'/assets/img/logo.png'} alt="logo" />
-                <h2 id="logo-name" className="animate__animated animate__fadeInUp">Demo React Owl Carousel</h2>
+                 <img src={'/assets/img/logo192.png'} alt="" />
+                <h2 id="logo-name" className="animate__animated animate__fadeInUp">Demo <span>
+                </span>React Owl Carousel</h2>
                 <h3>For Real State purposes</h3>
                 <a href="https://github.com/eliasmmata/slides-owl-react">Elías Moreno Mata</a>
             </div>
